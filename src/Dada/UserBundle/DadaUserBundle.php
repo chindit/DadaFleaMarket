183fa020656f68717b33383b07715b0c43b0e681
chindit <littletiger58@gmail.com>
2016-08-03 18:02:03 +0200

    Setting UserBundle as child of FOS

--- src/Dada/UserBundle/DadaUserBundle.php
+++ src/Dada/UserBundle/DadaUserBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DadaUserBundle extends Bundle
 {
+    /**
+     * Child of FOSUserBundle -> templates are located in this bundle
+     * @return string
+     */
+    public function getParent(){
+        return 'FOSUserBundle';
+    }
 }
